a3856a3e19d314f22c492cae3b4033249747f690
Rusbel <rusbeldelarosamunoz4.to.d@gmail.com>
2026-01-23 11:45:52 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const Username = document.getElementById("name");
-const Button = document.getElementById("btn");
+const Button = document.getElementById("submit");
 const Message = document.getElementById("mensaje");
 
 Button.addEventListener("click", () => {
     const name = Username.value.trim();
 
     if (name === "") {
-        Message.textContent = `Hey pon tu nombre pndj 😠`;
+        Message.textContent = `Hey pon tu nombre!!! ⚠️`;
     } else {
         Message.textContent = `Hola ${name}, bienvenido a el mejor JavaScript que veras hoy 😎`;
     }
